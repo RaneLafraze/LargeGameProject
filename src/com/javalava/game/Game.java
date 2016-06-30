73cf33e89cd52e6b7a1ab553002c6712e1348b6e
Rane Lafraze <lafrazerl@gmail.com>
2016-06-30 00:23:54 -0400

    Rename Main.java to Game.java and add main method

---
+++ src/com/javalava/game/Game.java
@@ -0,0 +1,9 @@
+package com.javalava.game;
+
+public class Game {
+	public static void main(String args[]) {
+		
+		
+		
+	}
+}
--- src/com/javalava/game/Main.java
+++
@@ -1,7 +0,0 @@
-package com.javalava.game;
-
-public class Main {
-	
-	
-	
-}
