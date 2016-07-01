56b0c46185599cf590f726d3f39f1d6b1aa5332f
Rane Lafraze <lafrazerl@gmail.com>
2016-06-30 23:01:07 -0400

    Write the majority of this class. Add final variable for the window title. Write a body for the setup method, and the init method.

--- src/com/javalava/game/GameFrame.java
+++ src/com/javalava/game/GameFrame.java
@@ -2,8 +2,15 @@ package com.javalava.game;
 
 import javax.swing.JFrame;
 
+
 public class GameFrame extends JFrame {
 	
+	private static final long serialVersionUID = 1L;
+	
+	// Caps since it is final
+	private final String WINDOW_TITLE = "LargeGame ALPHA 0.0.3";
+	
+	private GamePanel panel = new GamePanel();
 	
 	/**
 	 * The main frame for the game. This should be initialized
@@ -13,16 +20,44 @@ public class GameFrame extends JFrame {
 	 */
 	public GameFrame() {
 		
+		
+		setup();
 	}
 	
+	
 	/**
-	 * This method will initialize the JFrame. Operations within
+	 * This method will setup the JFrame. Operations within
 	 * would include setting visibility, the location and
 	 * dimensions of the frame, the title, etc.
 	 * 
 	 * @author Rane Lafraze
 	 */
+	public void setup() {
+		
+		// +22 accounts for the application bar at the top of the window
+		this.setSize(GlobalVariables.windowWidth, GlobalVariables.windowHeight + 22);
+		this.setTitle(WINDOW_TITLE);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		
+		// Add mouse and key listeners here
+		this.add("Center", panel);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+	}
+	
+	/**
+	 * "Begins" the game. Basically, it starts the main mechanics
+	 * and state engine of the program and does some cleaning up,
+	 * such as setting the window's visibility to true.
+	 * 
+	 * @author Rane Lafraze
+	 */
 	public void init() {
+		
+		this.setVisible(true);
+		
+		panel.begin();
 		
 	}
 	
