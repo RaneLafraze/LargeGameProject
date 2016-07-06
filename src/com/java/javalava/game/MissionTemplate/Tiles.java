c7758e476d5ad59b4dea45ed590f7dd83a73e18e
David <David@David>
2016-07-06 00:34:52 -0400

    tiles

---
+++ src/com/java/javalava/game/MissionTemplate/Tiles.java
@@ -0,0 +1,25 @@
+package com.java.javalava.game.MissionTemplate;
+
+public class Tiles {
+
+	int height;
+	int width;
+	
+	/**
+	 * This sets the height and width of the grid.
+	 * @param width
+	 * @param height
+	 */
+	
+	public void setDimensions(int height,int width) {
+		
+	}
+	
+	public int getHeight() {
+		return this.height;
+	}
+	public int getWidth() {
+		return this.width;
+	}
+	
+}
