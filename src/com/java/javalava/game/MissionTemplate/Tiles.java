bc972b76c6566a834d7849faf58973df0407627f
David <David@David>
2016-07-08 00:31:07 -0400

    grid was added

--- src/com/java/javalava/game/MissionTemplate/Tiles.java
+++ src/com/java/javalava/game/MissionTemplate/Tiles.java
@@ -42,27 +42,10 @@ public class Tiles {
 	 */
 	
 	public void setDimensions(int w,int h) {
-		height = h;
-		width = w;
-		
+
+	height = h;
+	width = w;
 	}
-
-
-
-	public void drawGrid() {
-	
-	TestState teststate = new TestState();
-	
-	grasstile = ImageHandler.getImage("assets/images/misc/Grass.png");
-	
-	int ycount = 20;
-	//if it has 5 height, this will generate 5 rows
-	for(int g=0;g<=this.height;g++) {
-		
-		ImageHandler.overlayImage(GlobalVariables.screen, grasstile, 20, ycount, 20, 20);
-		ycount = ycount + 10;
-	}
-}
 	
 	
 	public int getHeight() {
@@ -70,28 +53,34 @@ public class Tiles {
 	}
 	public int getWidth() {
 		return this.width;
-<<<<<<< HEAD
-=======
 	}
 	
 	
 	public void drawGrid() {
 		
 		int y = 0;
+		int shiftgridy = 30;
 		
 		grasstile = ImageHandler.getImage("assets/images/misc/Grass.png");
 		
+	
+		while(this.width > 0) {	
 		for(int g=0;g<=this.height;g++) {
-			
-			ImageHandler.overlayImage(GlobalVariables.screen, grasstile, 20, y, 20, 20);
-			
+			//x position y position
+			ImageHandler.overlayImage(GlobalVariables.screen, grasstile, 400 + shiftgridy, 250 + y, 30, 30);
 			y = y + 20;
 			
+			
+			}
+			y=0;
+			this.width--;
+			shiftgridy = shiftgridy + 30;
 		}
+		
+
 		
 		GlobalVariables.screen = ImageHandler.overlayImage(GlobalVariables.screen, grasstile, 20, 20, 20, 20);
 		
->>>>>>> refs/remotes/largegame/master
 	}
 		
 }
--- src/com/javalava/game/StateManager.java
+++ src/com/javalava/game/StateManager.java
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-package com.javalava.game;
-
-public class StateManager implements Runnable {
-	
-	TestState GameState;
-	
-	/**
-	 * Manages the possible states of the game.
-	 * 
-	 * @author Rane Lafraze
-	 */
-	public StateManager() {
-		
-		// Initialize the states here
-		GameState = new TestState();
-		
-	}
-	
-	// This is where the states will be checked and ran
-	public void run() {
-		
-		while(GlobalVariables.state > -1) {
-			
-			if(GlobalVariables.state == 1) {
-				
-				GameState.initalizeObjects();
-				while(GlobalVariables.state == 1) {
-					GameState.update();
-					delay(5); // For stability
-				}
-				
-			} // Add more states here
-			
-			
-		}
-	}
-	
-	/**
-	 * Delays, or waits, for the given amount of milliseconds.
-	 * 
-	 * 
-	 * @param millis
-	 * the time, in milliseconds (1 / 1000 of second) to delay
-	 * 
-	 * @author RaneLafraze
-	 */
-	private void delay(int millis) {
-		try {
-			Thread.sleep(millis);
-		} catch(InterruptedException ie) {
-			ie.printStackTrace();
-		}
-	}
-	
-}
-=======
 package com.javalava.game;
 
 public class StateManager implements Runnable {
@@ -110,4 +53,3 @@ public class StateManager implements Runnable {
 	}
 	
 }
->>>>>>> refs/remotes/largegame/master
